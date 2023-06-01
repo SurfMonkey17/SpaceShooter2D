3ed9b3d62200267b299f04dec3ecb1116c110185
Sherry Fisher <sherryw17@gmail.com>
2023-06-01 16:01:15 -0600

    completed player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f; //use underscore before variable name to indicate private
+
     // Start is called before the first frame update
     void Start()
     {
     //take the current position = new position (0, 0, 0)
     transform.position = new Vector3(0, 0, 0);     
     }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right);      
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //new Vector3(1, 0, 0) * horizontalInput * speed  * real time 
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        //restrict player movement on y axis to -3.8f and 0
+
+        if(transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if(transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+
+        }
+
+        //restrict player movement to between 11 and -11. Player will wrap to other side when limit is reached. 
+
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
     }
 }
