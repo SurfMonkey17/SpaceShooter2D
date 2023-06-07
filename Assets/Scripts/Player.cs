d3cd158ef99e30bafcac3c3b9a3827f15a242e71
Sherry Fisher <sherryw17@gmail.com>
2023-06-06 21:30:36 -0600

    Added laser

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 3.5f; //use underscore before variable name to indicate private
+    private float _speed = 3.5f; 
+    
+    
+    
+    //use underscore before variable name to indicate private
 
     // Start is called before the first frame update
     void Start()
