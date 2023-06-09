de3f4be201414f0848c94d920a5879962c86d858
Sherry Fisher <sherryw17@gmail.com>
2023-06-09 12:02:48 -0600

    laser fire added

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    //speed variable of 8
+
+    [SerializeField]
+    private float _speed = 8f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //translate laser up infinitely. 
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        //if laser position >12 on y, destroy object
+        if (transform.position.y > 12)
+            Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 3.5f; 
-    
-    
-    
-    //use underscore before variable name to indicate private
+    private float _speed = 3.5f;
+    [SerializeField] 
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
 
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,25 +24,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalculateMovement() 
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         //new Vector3(1, 0, 0) * horizontalInput * speed  * real time 
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+       
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
 
         //restrict player movement on y axis to -3.8f and 0
 
-        if(transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if(transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-
-        }
-
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0));
+        
         //restrict player movement to between 11 and -11. Player will wrap to other side when limit is reached. 
 
         if (transform.position.x > 11)
@@ -50,6 +54,13 @@ public class Player : MonoBehaviour
         else if (transform.position.x < -11)
         {
             transform.position = new Vector3(11, transform.position.y, 0);
+        }
+    }
+    void FireLaser()
+    {
+        {
+            _canFire = Time.time + _fireRate;
+            Object.Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
         }
     }
 }
